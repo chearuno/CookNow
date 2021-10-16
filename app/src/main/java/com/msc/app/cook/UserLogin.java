4668d555d2aa295457613c6e5548ea8fcc623e63
dushan-bond <dushan@bond.tech>
2021-10-17 01:27:19 +0530

    feat_userLogin&Reg_V2

--- app/src/main/java/com/msc/app/cook/UserLogin.java
+++ app/src/main/java/com/msc/app/cook/UserLogin.java
@@ -2,13 +2,43 @@ package com.msc.app.cook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.BreakIterator;
 
 public class UserLogin extends AppCompatActivity {
+    private EditText userEmail;
+    private EditText userPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+        userEmail = findViewById(R.id.editEmailAddress);
+        userPassword = findViewById(R.id.editPassword);
+        btnLogin = findViewById(R.id.loginBtn);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if(userEmail.toString().equals("testUser@gmail.com")
+                        && userPassword.toString().equals("testPassword")){
+                    Toast.makeText(UserLogin.this, "Login Successfull", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(UserLogin.this, "Login failed. Please check username and password again", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/msc/app/cook/UserRegistration.java
+++ app/src/main/java/com/msc/app/cook/UserRegistration.java
@@ -2,13 +2,37 @@ package com.msc.app.cook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class UserRegistration extends AppCompatActivity {
+    private EditText userName;
+    private EditText userEmail;
+    private EditText userPassword;
+    private Button btnCreateUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_registration);
+
+        userName = findViewById(R.id.editPersonName);
+        userEmail = findViewById(R.id.editEmailAddress);
+        userPassword = findViewById(R.id.editPassword);
+        btnCreateUser = findViewById(R.id.registerBtn);
+
+        btnCreateUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserRegistration.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
 }
