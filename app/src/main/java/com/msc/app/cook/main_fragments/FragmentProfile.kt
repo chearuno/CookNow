3c0242139b211ed3b3f9eb777bb7085da46eb1a7
Anushka-H <anushkah2010@gmail.com>
2021-10-31 07:09:18 +0530

    updates to profile

--- app/src/main/java/com/msc/app/cook/main_fragments/FragmentProfile.kt
+++ app/src/main/java/com/msc/app/cook/main_fragments/FragmentProfile.kt
@@ -8,6 +8,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.firestore.FirebaseFirestore
@@ -49,7 +51,9 @@ class FragmentProfile : Fragment() {
             requireActivity().getSharedPreferences("MyPrefsFile", 0)
         val userId = prefs.getString("loggedUserId", "").toString()
 
-        db!!.collection("User").whereEqualTo("userId", userId.toInt())
+
+
+        db!!.collection("User").whereEqualTo("id", userId.toLong())
             .get()
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
@@ -62,84 +66,29 @@ class FragmentProfile : Fragment() {
                         val lastName = documentData["lastName"]
                         val email = documentData["email"]
                         val password = documentData["password"]
-                        val favList: ArrayList<*> = documentData["favList"] as ArrayList<*>
 
-                        val storageRef = storage!!.reference
+                        val fullname = view.findViewById(R.id.userFullName) as TextView
+                        fullname.setText(firstName.toString()+" "+lastName.toString());
 
-//
-                        val item = ItemUser()
-                        item.id = id as Long?
-                        item.firstName = firstName as String?
-                        item.lastName = lastName as String?
-                        item.email = email as String?
-                        item.password = password as String?
-                        item.fullData = documentData
-                        itemList.add(item)
-
-                        val editText = view.findViewById(R.id.userFirstName) as TextInputEditText
-                        editText.setText("Hello world!");
-                        //view.userFirstName.setText(firstName.toString());
+                        val fname = view.findViewById(R.id.userFirstName) as TextInputEditText
+                        fname.setText(firstName.toString());
+                        val lname = view.findViewById(R.id.userLastName) as TextInputEditText
+                        lname.setText(lastName.toString());
+                        val emailval = view.findViewById(R.id.userEmail) as TextInputEditText
+                        emailval.setText(email.toString());
+                        val pass = view.findViewById(R.id.userPassword) as TextInputEditText
+                        pass.setText(password.toString());
                         Log.e("Doc", "Success getting user: ")
-//
                     }
-
-
                 } else {
                     Log.e("Doc", "Error getting documents: ", task.exception)
-//                    progressHUD!!.dismiss()
-//                    progressHUD!!.dismiss()
-//                    emptyView?.visibility = View.VISIBLE
-//                    recyclerView?.visibility = View.GONE
                 }
             }
-
-//        db!!.collection("User").whereEqualTo("userId", userId.toInt())
-//            .get()
-//            .addOnSuccessListener {
-//                Log.i("firebase", "Got value ${it.documents}")
-//
-//                it.documents.map
-//                const docSnapshots = querysnapshot.docs;
-//                for (var i in docSnapshots) {
-//                const doc = docSnapshots[i].data();
-//                 Check for your document data here and break when you find it
-//            }
-//        }.addOnFailureListener {
-//        Log.e("firebase", "Error getting data", it)
-//        }
-//                    snapshot, e ->
-//                    if (e != null) {
-//                        Log.w(TAG, "Listen failed.", e)
-//                        return@addSnapshotListener
-//                    }
-//                    if (snapshot != null) {
-//                        Log.d(TAG, "Current data: ${snapshot}")
-//                    } else {
-//                        Log.d(TAG, "Current data: null")
-//                    }
-//                }
-//
-//                    task ->
-//                if (task.isSuccessful) {
-//                    for (document in task.result!!) {
-//                        Log.e("Doc", document.id)
-//                        val documentData = document.data
-//                        val firstName = documentData["firstName"]
-//                        val lastName = documentData["lastName"]
-//                        val id = documentData["id"]
-//                        view.firstNameTextEdit.setText(firstName.toString());
-//                    }
-//                } else {
-//                    Log.e("Doc", "Error getting documents: ", task.exception)
-//                }
-//            }
 
 
         val updateButtonClick = view.findViewById(R.id.updateBtn) as Button
         // set on-click listener
         updateButtonClick.setOnClickListener {
-
-
             val putData: java.util.HashMap<String, Any> = java.util.HashMap()
             putData["firstName"] = view.userFirstName.text.toString()
             putData["lastName"] = view.userLastName.text.toString()
@@ -148,9 +97,16 @@ class FragmentProfile : Fragment() {
 
             db!!.collection("User").document(userId)
                 .update(putData)
-                .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
-                .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
-            //Toast.makeText(this@FragmentProfile, "You clicked me.", Toast.LENGTH_SHORT).show()
+                .addOnSuccessListener {
+                    Log.d(TAG, "DocumentSnapshot successfully written!")
+                    Toast.makeText(activity, "Update successful", Toast.LENGTH_SHORT).show()
+                    val fullname = view.findViewById(R.id.userFullName) as TextView
+                    fullname.setText("${view.userFirstName.text.toString()} ${view.userLastName.text.toString()}");
+                }
+                .addOnFailureListener {
+                        e -> Log.w(TAG, "Error writing document", e)
+                    Toast.makeText(activity, "Error updating", Toast.LENGTH_SHORT).show()
+                }
         }
         return view
     }
